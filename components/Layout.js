32dff1f9a09e89ed6d6254e6a58f0302e6e501f3
Daan De Deckere <daandedeckere@gmail.com>
2020-10-15 15:09:37 +0700

    Add page title

--- components/Layout.js
+++ components/Layout.js
@@ -1,12 +1,18 @@
+import Head from "next/head";
 import Navbar from "./Navbar";
 
 const Layout = ({ children }) => {
   return (
-    <div className="flex w-full h-screen">
-      <Navbar />
+    <>
+      <Head>
+        <title>DODO Dashboard</title>
+      </Head>
+      <div className="flex w-full h-screen">
+        <Navbar />
 
-      <div className="p-12 overflow-auto flex-1">{children}</div>
-    </div>
+        <div className="p-12 overflow-auto flex-1">{children}</div>
+      </div>
+    </>
   );
 };
 
