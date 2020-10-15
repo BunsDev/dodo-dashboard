3e2af67a1d73526b01d49d3a74a3153d226b1a59
Daan De Deckere <daandedeckere@gmail.com>
2020-10-15 22:12:13 +0700

    Style navbar and add home icon

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,4 +1,6 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
+import HomeIcon from "./icons/home.svg";
 
 const navLinks = [
   {
@@ -15,23 +17,66 @@ const navLinks = [
   },
 ];
 
-const Navbar = () => (
-  <nav className="bg-white h-full py-12 w-24 text-center">
-    <img
-      alt="DODO Logo"
-      className="w-16 rounded-full mb-12 mx-auto"
-      src="/images/dodo.png"
-    />
-    <ul>
-      {navLinks.map((navLink) => (
-        <li key={navLink.href}>
-          <Link href={navLink.href}>
-            <a>{navLink.label}</a>
-          </Link>
-        </li>
-      ))}
-    </ul>
-  </nav>
-);
+const Navbar = () => {
+  const router = useRouter();
+
+  return (
+    <>
+      <style jsx global>{`
+        .nav-list li {
+          @apply mb-2;
+        }
+
+        .nav-link {
+          @apply text-gray-500 flex flex-col text-sm items-center justify-center h-20 rounded;
+        }
+
+        .nav-link .icon {
+          @apply mt-1;
+        }
+
+        .nav-link .icon path {
+          @apply fill-current;
+        }
+
+        .nav-link:hover {
+          @apply text-gray-700 bg-yellow-200;
+        }
+
+        .nav-link.active {
+          @apply text-black bg-yellow-400;
+        }
+      `}</style>
+
+      <nav className="bg-white h-full py-12 w-24 text-center">
+        <img
+          alt="DODO Logo"
+          className="w-16 rounded-full mb-12 mx-auto"
+          src="/images/dodo.png"
+        />
+        <ul className="nav-list mx-2">
+          {navLinks.map((navLink) => (
+            <li key={navLink.href}>
+              <Link href={navLink.href}>
+                <a
+                  className={
+                    router.pathname === navLink.href
+                      ? "nav-link active"
+                      : "nav-link"
+                  }
+                >
+                  <div className="icon">
+                    <HomeIcon />
+                  </div>
+                  <div className="mt-1">{navLink.label}</div>
+                </a>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </>
+  );
+};
 
 export default Navbar;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Layout from "../components/Layout";
-import { ALL_POOLS_QUERY } from "../components/Pools";
 import Home from "../components/Home";
 import { initializeApollo } from "../lib/apolloClient";
 
@@ -8,20 +7,5 @@ const IndexPage = () => (
     <Home />
   </Layout>
 );
-
-export async function getStaticProps() {
-  const apolloClient = initializeApollo();
-
-  await apolloClient.query({
-    query: ALL_POOLS_QUERY,
-  });
-
-  return {
-    props: {
-      initialApolloState: apolloClient.cache.extract(),
-    },
-    revalidate: 1,
-  };
-}
 
 export default IndexPage;
