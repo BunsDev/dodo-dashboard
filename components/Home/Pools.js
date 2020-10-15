da8e098e151cf7917f07245bad81da4971673548
Daan De Deckere <daandedeckere@gmail.com>
2020-10-15 16:11:40 +0700

    Add liquidity and volume

--- components/Home/Pools.js
+++ components/Home/Pools.js
@@ -14,24 +14,45 @@ export const ALL_POOLS_QUERY = gql`
         id
         symbol
         name
+        totalLiquidity
+        tradeVolumeUSD
       }
     }
   }
 `;
 
+const Pool = ({ pair }) => (
+  <tr>
+    <td>
+      {pair.baseToken.symbol}-{pair.quoteToken.symbol}
+    </td>
+    <td>{pair.quoteToken.totalLiquidity}</td>
+    <td>{pair.quoteToken.tradeVolumeUSD}</td>
+  </tr>
+);
+
 export default function Pools() {
   const { loading, error, data } = useQuery(ALL_POOLS_QUERY);
 
+  if (error) return <ErrorMessage message="Error loading posts." />;
+  if (loading) return <div>Loading…</div>;
+
   const { pairs } = data;
 
-  if (error) return <ErrorMessage message="Error loading posts." />;
-  if (loading && !loadingMorePosts) return <div>Loading…</div>;
-
   return (
-    <section>
-      {pairs.map((pair) => (
-        <div>{pair.id}</div>
-      ))}
-    </section>
+    <table className="table-auto w-full">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Liquidity</th>
+          <th>Volume (24hr)</th>
+        </tr>
+      </thead>
+      <tbody>
+        {pairs.map((pair) => (
+          <Pool key={pair.id} pair={pair} />
+        ))}
+      </tbody>
+    </table>
   );
 }
