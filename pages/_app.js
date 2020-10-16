2bb8886d1d4a89d0dc39853244f73086ec21bca6
Daan De Deckere <daandedeckere@gmail.com>
2020-10-16 18:42:50 +0700

    Add favicons

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import { ApolloProvider } from "@apollo/client";
 import { useApollo } from "../lib/apolloClient";
 import "../styles/tailwind.css";
@@ -7,6 +8,20 @@ export default function App({ Component, pageProps }) {
 
   return (
     <ApolloProvider client={apolloClient}>
+      <Head>
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+      </Head>
       <Component {...pageProps} />
       <link
         href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap"
